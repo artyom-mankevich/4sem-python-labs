b398efa0385a6ab29d49e631076ff843b53fa4f8
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-08-06 18:57:23 +0300

    Added __str__ methods for all models

--- lab3/store/models.py
+++ lab3/store/models.py
@@ -10,10 +10,16 @@ class User(models.Model):
     email = models.CharField(max_length=250, unique=True)
     phone_number = models.CharField(max_length=15, unique=True)
 
+    def __str__(self):
+        return ' '.join((str(self.id), self.first_name, self.second_name, self.email, self.phone_number))
+
 
 class Order(models.Model):
     order_date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ' '.join((str(self.id), str(self.user), str(self.order_date)))
 
 
 class OrderItem(models.Model):
@@ -21,6 +27,10 @@ class OrderItem(models.Model):
     unit_price = models.IntegerField("product price * quantity")
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ' '.join((str(self.id), str(self.quantity), str(self.unit_price),
+                         str(self.order), str(self.product)))
 
 
 class Product(models.Model):
@@ -35,3 +45,8 @@ class Product(models.Model):
     artist_death = models.DateField()
     artist_nationality = models.CharField(max_length=50)
     art_description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return ' '.join((str(self.id), self.category, self.size,
+                         self.price, self.title, self.art_dating,
+                         self.art_id, self.artist))
