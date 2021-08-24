9fc8d7bad75dfbde227f514e8677c533d411056c
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-08-24 20:42:12 +0300

    Added new urls, changed store app url from '/store/' to '/'

--- lab3/store/urls.py
+++ lab3/store/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 app_name = 'store'
 urlpatterns = [
-    # ex: /store/
     path('', views.IndexView.as_view(), name='index'),
-    # ex: /store/products/
     path('products/', views.ProductsView.as_view(), name='products'),
-    # ex: /store/products/5
-    path('products/<int:pk>', views.ProductByIdView.as_view(), name='product by id')
+    path('products/<int:pk>', views.ProductByIdView.as_view(), name='product by id'),
+    path('register/', views.register, name='register'),
+    path('cart/', views.cart, name='cart'),
+    path('update_item/', views.update_item, name='update_item'),
+    path('save_order/', views.save_order, name='save_order')
 ]
